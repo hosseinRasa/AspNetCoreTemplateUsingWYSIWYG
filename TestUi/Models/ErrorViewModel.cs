7e1bdc0aeff327754cd606783a86fb1a06255f6a
unknown <hosseinrasa7@google.com>
2023-02-03 08:20:17 -0800

    'first'

---
+++ TestUi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestUi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
